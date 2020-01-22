0dc56685283b1a0d66a86190bff2df18d48240cb
Freewind <nowindlee@gmail.com>
2020-01-22 11:57:43 +0800

    php-hello-world-demo

---
+++ README.md
@@ -0,0 +1,6 @@
+Php Hello World Demo
+====================
+
+```
+php hello.php
+```
---
+++ hello.php
@@ -0,0 +1,9 @@
+<?php
+echo('Hello PHP!');
+echo('/aaa.php');
+$xx = array(
+  'aa' => '11',
+  'bb' => '22'
+);
+echo($xx);
+?>
